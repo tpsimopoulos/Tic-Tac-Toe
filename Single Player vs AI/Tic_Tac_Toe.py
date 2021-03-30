f03bbc8d5119922f8a6611170019cdc3d519682c
Ted Simopoulos <tpsimopoulos@gmail.com>
2021-03-30 16:01:58 -0400

    Finished AI vs Human/RandomPlayer implementation.

--- Single Player vs AI/Tic_Tac_Toe.py
+++ Single Player vs AI/Tic_Tac_Toe.py
@@ -1,15 +1,16 @@
-from player import HumanPlayer, ComputerPlayer, AiPlayer
+from player import HumanPlayer, RandomPlayer, AiPlayer
 import time
 
 class Tic_Tac_Toe:
-    def __init__(self, HumanPlayer, OtherPlayer):
-        self.HumanPlayer = HumanPlayer
-        self.OtherPlayer = OtherPlayer
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
         self.board = [['' for j in range(3)] for i in range(3)]
-        self.available_moves = self.get_available_moves(self.board)
         self.winner = None
+        self.tie = None
 
     def print_board(self, board):
+        """Prints a representation of the current board."""
         print('\n')
         print('-------------------')
         print('\n')
@@ -24,79 +25,113 @@ class Tic_Tac_Toe:
                 else:
                     print(board[i][j], end =" ")
     
+
     def get_available_moves(self, board):
+        """Returns all available moves on given board."""
         available_moves = {}
         counter = 0
         for i in range(len(board)):
             for j in range(len(board)):
                 if board[i][j] == '':
                     available_moves[counter] = (i,j)
-                    counter+=1
+                counter+=1
         return available_moves
 
-    def make_move(self, board, move, whose_move):
-        if isinstance(whose_move, str):
-            row, col = move
-            board[row][col] = whose_move
+
+    def return_current_player_letter(self, current_player):
+         # If called from minimax.
+        if isinstance(current_player, str): 
+            pass
         else:
-            row, col = move
-            board[row][col] = whose_move.letter
+            # If called from actual gameplay.
+            current_player = current_player.letter 
+        return current_player
+
+        
+    def make_move(self, board, move, current_player):
+        """Given a spot on the board and a player, makes a move for that player."""
+        row, col = move
+
+        # Returning the letter of the current player. 
+        current_player = self.return_current_player_letter(current_player)
+
+        if board[row][col] == '':
+            board[row][col] = current_player
+            # Checking for winner or tie after move is made. 
+            self.check_for_winner(self.board, current_player)
+            self.check_for_tie(self.board, current_player)
+        else:
+            return False
+
 
     def empty_square_count(self, board):
-        return len([(i,j) for j in range(len(board)) for i in range(len(board))])
+        """Returns all the empty spaces currently on the board."""
+        return len([(i,j) for j in range(len(board)) for i in range(len(board)) if board[i][j]==''])
 
-    def check_for_winner(self, board, whose_move):
-        #check rows
+
+    def check_for_winner(self, board, current_player):
+        """
+        Checks the board for any winning combination. If winning combination is found,
+        function assigns current_player as the winner.
+        """
+
+        # Check rows.
         for i in range(len(board)):
-            if all(board[i][j] == whose_move.letter for j in range(3)):
-                return 'Winner'
+            if all(board[i][j] == current_player for j in range(3)):
+                self.winner = current_player
+                return True
             
-        #check cols
+        # Check columns.
         for i in range(len(board)):
-            if all(board[j][i] == whose_move.letter for j in range(3)):
-                return 'Winner'
+            if all(board[j][i] == current_player for j in range(3)):
+                self.winner = current_player
+                return True
 
-        #check diagnols
-        if all([board[i][j] == whose_move.letter for i,j in [(0,0),(1,1),(2,2)]]):
-            return 'Winner'
-        if all([board[i][j] == whose_move.letter for i,j in [(0,2),(1,1),(2,0)]]):
-            return 'Winner'
-        
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] != '':
-                    continue
-                else:
-                    return False
-        return 'Tie'
+        # Check diagnols.
+        if all([board[i][j] == current_player for i,j in [(0,0),(1,1),(2,2)]]):
+            self.winner = current_player
+            return True
+        if all([board[i][j] == current_player for i,j in [(0,2),(1,1),(2,0)]]):
+            self.winner = current_player
+            return True
     
+
+    def check_for_tie(self, board, current_player):
+        """Checks the board for a tie."""
+        
+        # Check tie. 
+        if all(True if board[i][j] != '' else False for i in range(len(board)) for j in range(len(board))):
+            self.tie = True 
+            return True
+    
+
     def play_game(self):
-        winner_not_found = True
-        whose_move = self.HumanPlayer
-        while winner_not_found:
-            time.sleep(.5)
+        """This function serves as the main game loop."""
+        current_player = self.player_1
+        
+        # Only prints board out before selection if it's HumanPlayer's turn
+        if type(current_player).__name__ == 'HumanPlayer':
             self.print_board(self.board)
-            move = whose_move.get_move(self)
-            self.make_move(self.board, move, whose_move)
-            if self.check_for_winner(self.board, whose_move)=='Winner':
-                self.print_board(self.board)
-                print('\n')
-                print(f'Player {whose_move.letter} wins!')
-                self.winner = whose_move.letter
-                winner_not_found = False
-            elif self.check_for_winner(self.board, whose_move)=='Tie':
-                self.print_board(self.board)
+        
+        while not self.winner and not self.tie:
+            time.sleep(.5)
+
+            # Gets the move of player whose turn it is.
+            move = current_player.get_move(self) 
+
+            # Executes move returned by the current player
+            self.make_move(self.board, move, current_player)
+            self.print_board(self.board)
+
+            if self.winner:
+                print(f'Player {self.winner} wins!')
+            elif self.tie:
                 print('It\'s a tie!')
-                self.winner = whose_move.letter
-                winner_not_found = False
-            else:
-                if whose_move == self.HumanPlayer:
-                    whose_move = self.OtherPlayer
-                else:
-                    whose_move = self.HumanPlayer
-            continue
+
+            # If there is no winner or tie, switch whose turn it is.
+            current_player = self.player_1 if current_player == self.player_2 else self.player_2
 
 
 if __name__ == '__main__':
-    t = Tic_Tac_Toe(HumanPlayer('X'), AiPlayer('O'))
+    t = Tic_Tac_Toe(AiPlayer('O'), RandomPlayer('X'))
     t.play_game()
--- Single Player vs AI/player.py
+++ Single Player vs AI/player.py
@@ -6,84 +6,102 @@ class Player:
     def __init__(self, letter):
         self.letter = letter
 
-class ComputerPlayer(Player):
+class RandomPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        rand_choice = random.choice([k for k in game.available_moves.keys()]) #selecting random key
-        if any([True if k == rand_choice else False for k,v in game.available_moves.items()]):
-            move_coordinate = game.available_moves[rand_choice]
-            del game.available_moves[rand_choice]
+        """This function selects a random spot on the board from the available moves."""
+
+        # Selecting a random available spot on the board given a game state.
+        available_moves = game.get_available_moves(game.board)
+        rand_choice = random.choice([k for k in available_moves.keys()]) 
+        if any([True if k == rand_choice else False for k,v in available_moves.items()]):
+            move_coordinate = available_moves[rand_choice]
         return move_coordinate
+
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
+        """This function takes the user's spot selection and returns the coordinates."""
+        available_moves = game.get_available_moves(game.board)
         move_is_valid = False
-        move = None
         while not move_is_valid:
             try:
                 print('\n')
                 move = int(input(f'{self.letter}\'s turn. Make a move (0-8): '))
-                if move not in game.available_moves:
+                if move not in available_moves:
                     raise ValueError
                 move_is_valid = True
-                move_coordinate = game.available_moves[move]
-                del game.available_moves[move]
+                move_coordinate = available_moves[move]
             except ValueError:
                 print('Invalid selection. Please try another number (0-8): ')
         return move_coordinate
-
+    
 class AiPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        if len(game.available_moves) == 9:
-            rand_choice = random.choice([k for k in game.available_moves.keys()])
-            move_coordinate = game.available_moves[rand_choice]
-            del game.available_moves[rand_choice]
+        """This function returns the AI player's move."""
+
+        # Selecting a random spot on the board if all spaces are available.
+        available_moves = game.get_available_moves(game.board)
+        if len(available_moves) == 9:
+            rand_choice = random.choice([k for k in available_moves.keys()])
+            move_coordinate = available_moves[rand_choice] 
         else:
-            #get square from minimax
+            # If board isn't empty, return optimal move for AI player via minimax.
             move_coordinate = self.minimax(game, self.letter)['position']
+            
         return move_coordinate
 
-    def minimax(self, game, player):
-        max_player = self.letter
-        other_player = 'O' if player == 'X' else 'X'
+    def minimax(self, game_state, current_move):
+        """
+        This function enables the AI player to simulate all possible
+        game scenarios via the minimax algorithm and return the optimal next move
+        for it to make in the real game. 
+        """
+        max_player = self.letter 
+        previous_move = 'O' if current_move == 'X' else 'X'
 
-        #check if previous move is winner
-        if game.winner == other_player:
-            return {'position': None,
-                    'score': 1 * (game.empty_square_count() + 1) if other_player == maxplayer else -1 * (game.empty_square_count() + 1)}
-
-        elif not game.empty_square_count(game.board):
+        # If the previous move was a winner, return the score of the winning result. 
+        if game_state.winner == previous_move:
+            return {'position': None, 'score': 1 * (game_state.empty_square_count(game_state.board) + 1) if previous_move == max_player else -1 * (game_state.empty_square_count(game_state.board) + 1)}
+        # If the board is full but no winners, return a score of 0. 
+        elif game_state.empty_square_count(game_state.board) == '0':
             return {'position': None, 'score': 0}
 
-        if player == max_player:
-            best = {'position': None, 'score': -math.inf}
+        # Assigning how best score starts given whose turn it is (maximizer or minimizer's).
+        if current_move == max_player:
+            best_score = {'position': None, 'score': -math.inf} 
         else:
-            best = {'position': None, 'score': math.inf}
+            best_score = {'position': None, 'score': math.inf} 
 
-        for key, possible_move in game.available_moves.items():
-            game.make_move(game.board, possible_move, player)
-            #simulate what game would look like after the move is made
-            sim_score = self.minimax(game, other_player)
-            game.board[idx] = possible_move
-            row, col = possible_move
-            #undo move
-            game.board[row][col] = ''
-            game.winner = None
-            sim_score['position'] = possible_move
+        # Makes all available moves alternating players with each move made.
+        for key, move in game_state.get_available_moves(game_state.board).items():
+            game_state.make_move(game_state.board, move, current_move)
+            sim_score = self.minimax(game_state, previous_move)
 
-            if player == max_player:
-                if sim_score['score'] > best['score']:
-                    best = sim_score
+            # If a winner/tie is returned, clear the board of the spot selected that achieved that result,
+            # , reset the game's winner to None, and assign the current board position to the simulated 
+            # score's position value.
+            row, col = move
+            game_state.board[row][col] = ''
+            game_state.winner = None
+            game_state.tie = None
+            sim_score['position'] = move
+            
+            # Replace the current best_score if the simulated score is more optimal for the current player. 
+            if current_move == max_player:
+                if sim_score['score'] > best_score['score']:
+                    best_score = sim_score
             else:
-                if sim_score['score'] < best['score']:
-                    best = sim_score
-        return best 
+                if sim_score['score'] < best_score['score']:
+                    best_score = sim_score
+
+        return best_score 
 
